6b917b0ba05c670b1b62735cf63c720e8bc727df
Bill Fern <bfern@nerdery.com>
2014-03-15 01:22:15 -0500

    bfern modifications

--- multeeplye.js
+++ multeeplye.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
-    $("#button").click(function(){
+    $('#multiply-control').click(function(){
         var v1 = $('#v1').val();	
         var v2 = $('#v2').val();	
         var total = v1 * v2;
         if (!isNaN(total)) {
-            $('#total').attr('value', total);
+            $('#total').val(total);
         } else { 
             alert('Whoops! Please only enter numbers in these fields.');
         }
